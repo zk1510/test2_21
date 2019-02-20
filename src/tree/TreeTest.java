902bc6fd211c101183eff61a4e29184622784a65
rose <rose@163.com>
2019-02-21 00:21:39 +0800

    first time update

---
+++ src/tree/TreeTest.java
@@ -0,0 +1,65 @@
+package tree;
+
+public class TreeTest {
+	public static void main(String[] args) {
+		Node root = new Node().init();
+		Node.sort(root);
+	}
+}
+
+class Node {
+	private char value;
+	private Node leftNode;
+	private Node rightNode;
+	
+	public Node init() {
+		Node D = new Node('D', null, null);
+		Node E = new Node('E', null, null);
+		Node B = new Node('B', D, E);
+		Node F = new Node('F', null, null);
+		Node C = new Node('C', F, null);
+		Node A = new Node('A', B, C);
+		return A;
+	}
+	public static void sort(Node node) {
+		if (node != null) {
+//			System.out.println(node.getValue());
+			if (node.getLeftNode() != null) {
+				sort(node.getLeftNode());
+			}
+//			System.out.println(node.getValue());
+			if (node.getRightNode() != null) {
+				sort(node.getRightNode());
+			}
+			System.out.println(node.getValue());
+		}
+	}
+	public char getValue() {
+		return value;
+	}
+	public void setValue(char value) {
+		this.value = value;
+	}
+	public Node getLeftNode() {
+		return leftNode;
+	}
+	public void setLeftNode(Node leftNode) {
+		this.leftNode = leftNode;
+	}
+	public Node getRightNode() {
+		return rightNode;
+	}
+	public void setRightNode(Node rightNode) {
+		this.rightNode = rightNode;
+	}
+	public Node(char value, Node leftNode, Node rightNode) {
+		super();
+		this.value = value;
+		this.leftNode = leftNode;
+		this.rightNode = rightNode;
+	}
+	public Node() {
+		super();
+	}
+	
+}
